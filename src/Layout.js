ce601b423bd755eec0a2326a78bbc6dc4740ea50
Dayniel <daynielbatista@gmail.com>
2021-02-26 00:20:09 +0000

    New project set up

---
+++ src/Layout.js
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div><h2>Testing</h2></div>
+    );
+}
+
+export default Layout; 
